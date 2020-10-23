0a4b9d895c017b29f20452e1f2775f4d85b8c763
farhanazam41 <39867162+farhanazam41@users.noreply.github.com>
2020-10-22 18:44:21 -0700

    adding github buttons back

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,14 +39,15 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'Front End/UI Developer with 3 years of hands-on experience efficiently coding websites and applications using modern HTML5, CSS3, and JavaScript, React js, Redux, Next js, Node js, GraphQL.'}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'Translate designs to front-end code, estimate time and costs, and determine developer requirements and specifications.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree ||
+                    'Collaborate with creative and development teams on the execution of ideas and projects.'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
--- src/mock/data.js
+++ src/mock/data.js
@@ -10,47 +10,86 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Syed Naqvi',
+  subtitle: 'I am the Front-End Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'upwork.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://gofile.io/d/ToNV3l', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    img: 'cloudsbay.jpg',
+    title: 'Online Electronics Store',
+    info:
+      'Advanced E-Commerce online electronic store website, Users can Sign up using Email and Password or social like Gmail.Users shop around different categories and items will go into the cart, cart updates automatically in real-time when there is a new item added.',
+    info2:
+      'When the user is done shopping, hit the checkout button to proceed to the payment through Stripe Payment API.Clean and simple e-commerce website using React, Sass as a Front-End and FIrebase as a Backend. Note: This is a demo personal project. More work on the website will be added as per client requirements ',
+    url: 'https://cloudsbay.com/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    img: 'homedecor.jpg',
+    title: 'E-Commerce Online Furniture Store',
+    info:
+      'Made using React.js, Next.js , Node.js, Semantic UI. Clean and simple online decor website, where users can shop for different home furniture products. Users can view a single product with a proper description. Users can view their account to see the order history.',
+    info2:
+      'Sign in & Sign up functionality. When users have done shopping, they can click the checkout button to proceed to the payment screen. Note: This is a personal demo project. More work and details will be added as per the client requirements.',
+    url: 'https://home-decor.now.sh/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    img: 'crown.jpg',
+    title: 'Clothing Store',
+    info:
+      'Simple E-Commerce online clothing website, Users can Sign up using Email and Password or social like Gmail. Users shop around different categories and items will go into the cart, cart updates automatically in real-time when there is a new item added.',
+    info2:
+      'When the user is done shopping, hit the checkout button to proceed to the payment through Stripe Payment API. Clean and simple e-commerce website using React, Sass as a Front-End and FIrebase as a Backend. Note: This is a demo personal project. More work on the website will be added as per client requirements.',
+    url: 'https://crwn-live.herokuapp.com/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'instagramclone.jpg',
+    title: 'Instagram Clone',
+    info:
+      'This porject is made using React as Front-End Liberary and Node.js as API BackEnd. Used beautiful material-design UI liberary ',
+    info2:
+      'For data storage used NoSQL MongoDB. User can Sign-up and create posts, upload pictures and follow other users just same like Real instagram features.',
+    url: 'http://instaclone55.herokuapp.com/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'smart-brain.jpg',
+    title: 'Smart-Brain App',
+    info:
+      'Its a Face detection web app, users can sign up and upload images & than detect faces in those images',
+    info2:
+      'Made with React as Front-End and Node.js as Backend API. Used most popular SQL database Postgres',
+    url: 'http://smart-brain.herokuapp.com/',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'spicekorner.jpg',
+    title: 'Food Ordering App',
+    info:
+      'Online Food ordering website made using React as a Front-End and Firebase as a Backend. Users can browse different food items and add them to there order list. When Users have done browsing they can simply hit the checkout button to get the food delivered.',
+    info2:
+      'Note: This is a personal demo project. More work and details will be added as per the client requirements.',
+    url: 'https://spicekorner.web.app/',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 ];
